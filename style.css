3eab2b2915324e55968924eb9ad19f54198ee82f
Monika <monikatn047@gmail.com>
2024-12-08 12:14:29 +0530

    Add files via upload

---
+++ style.css
@@ -0,0 +1,116 @@
+* {
+  box-sizing: border-box;
+}
+
+body {
+
+  background: #201c1a url('writing.jpg') no-repeat center;
+  background-position: 50%;
+  background-position-y: 25%;
+  font-family: 'Lato', sans-serif;
+  min-height: 80.5vh;
+  margin-top: -90px;
+}
+
+h1 {
+  text-align: center;
+  color: #fff
+}
+
+.container {
+  margin: auto;
+  padding: 70px;
+}
+
+.btn {
+  cursor: pointer;
+  background-color: darksalmon;
+  border: 0;
+  border-radius: 4px;
+  color: #fff;
+  font-size: 16px;
+  padding: 8px;
+}
+
+.btn:active {
+  transform: scale(0.98);
+}
+
+.btn-toggle {
+  display: block;
+  margin: auto;
+  margin-bottom: 20px;
+}
+
+.text-box {
+  width: 70%;
+  position: absolute;
+  top: 15%;
+  left: 50%;
+  transform: translate(-50%, -800px);
+  background-color: #333;
+  color: #fff;
+  padding: 20px;
+  border-radius: 5px;
+  transition: all 1s ease-in-out;
+}
+
+.text-box.show {
+  transform: translate(-50%, 0);
+}
+
+.text-box select {
+  background-color: darksalmon;
+  border: 0;
+  color: #fff;
+  font-size: 12px;
+  height: 30px;
+  width: 100%;
+}
+
+.text-box textarea {
+  border: 1px #dadada solid;
+  border-radius: 4px;
+  font-size: 16px;
+  padding: 8px;
+  margin: 15px 0;
+  width: 100%;
+  height: 150px;
+}
+
+.text-box .btn {
+  width: 100%;
+}
+
+.text-box .close {
+  float: right;
+  text-align: right;
+  cursor: pointer;
+}
+body {
+  background-color: rgb(9, 26, 51);
+  color: rgb(184, 5, 14);
+  font-family: Verdana, Geneva, Tahoma, sans-serif;
+  font-display: inherit;
+  min-height: 50vh;
+  overflow: hidden;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-size: cover;
+  background-position: bottom;
+  background-position-y: bottom 5px;
+}
+
+.button,
+button {
+  margin-top: 10px;
+  height: 50px;
+  width: 70px;
+
+}
+
+button,
+button {
+  background-color: #fff7
+}
